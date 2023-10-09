146f4ec7988613614f0bdaaa7a8aef62a3b803af
ilyass el harak <ilyasselharak@gmail.com>
2023-10-09 21:01:34 +0300

    fixed

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,23 +1,42 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-
+import React, { useMemo, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 export default function Header() {
+  const [menu, setMenu] = useState(0);
+  const hello = useMemo(() => menu, [menu]);
+
   return (
     <>
-      <div className=" md:w-[80%] mx-auto flex gap-x-5 items-center font-bold bg-gray-300 p-7 rounded-md">
-        <Link href={"/"}>
-          <Image
-            src={"/korashot-logo.png"}
-            width={200}
-            height={250}
-            alt="logo"
-          />
-        </Link>
-        <Link href={"/liveToday"}> مباريات اليوم</Link>
-        <Link href={"/liveScore"}>بث مباشر للمباريات</Link>
+      <div className=" md:w-[80%]  shadow-xl mx-auto  font-bold bg-gray-300 p-7 rounded-md">
+        <div className="md:justify-start flex gap-x-5 items-center">
+          <div
+            onClick={() => setMenu((prev) => prev + 1)}
+            className="text-3xl cursor-pointer"
+          >
+            <AiOutlineMenu /> {hello}
+          </div>
+          <Link href={"/"} className="mx-auto md:mx-0">
+            <Image
+              src={"/korashot-logo.png"}
+              width={200}
+              height={250}
+              alt="logo"
+            />
+          </Link>
+          <Link href={"/liveToday"} className=" hidden md:block">
+            مباريات اليوم
+          </Link>
+        </div>
+        <div className={`mt-14 ${menu ? "hidden" : "block"}`}>
+          <Link href={"/liveToday"} className="md:hidden block bg-gray-300">
+            مباريات اليوم
+          </Link>
+        </div>
       </div>
-      <div className="flex justify-center mt-14">
+
+      <div className="flex  cursor-pointer justify-center mt-14">
         <Image width={728} height={90} alt={"ads"} src={"/banner2.jpg"} />
       </div>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
+import Link from "next/link";
 import Header from "./components/Header";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [menu, setMenu] = useState(false);
+  console.log(menu);
   return (
     <>
-     
+      <main className=" bg-gray-300 p-4 mt-14">
+        <div className="md:w-[80%] mx-auto flex justify-between">
+          <div className="w-full px-5">
+            <button onClick={() => setMenu(!menu)}>click here</button>
+            <p className="font-bold text-xl ">
+              بث مباشر لمبارات كرة القدم بين سبورت
+            </p>
+
+            <div className=" mt-10 flex gap-4 flex-wrap ">
+              {Array.from({ length: 6 }).map((_, index) => (
+                <Link key={index} href={`/beinSport/${index + 1}`}>
+                  <div className=" bg-violet-600 min-w-[400px] text-white text-center py-6 rounded-lg">
+                    Bein Sport {index + 1}
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div>
+            <Image src={"/banner3.jpg"} width={300} height={600} alt={"ads"} />
+          </div>
+        </div>
+      </main>
+      <div className="flex justify-center mt-16">
+        <Image src={"/banner2.jpg"} width={726} height={68} alt={"ads"} />
+      </div>
     </>
   );
 }
